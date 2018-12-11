1ba6861e624ec50b2fddfeb7a0a8e3539464934c
zan <zan.kusterle@gmail.com>
2018-12-11 13:49:12 +0100

    cleanup

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Game from '@/components/Game'
-import Over from '@/components/Over'
 
 Vue.use(Router)
 
@@ -9,6 +8,5 @@ export default new Router({
   mode: 'history',
   routes: [
     { path: '/', name: 'Game', component: Game },
-    { path: '/over', name: 'Over', component: Over },
   ]
 })
